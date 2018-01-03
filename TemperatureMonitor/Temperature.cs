75b2518a0639afafad3bdd9d3747c6e0416ba23c
newli <newlifechou.outlook.com>
2018-01-03 21:30:30 +0800

    add new help class

---
+++ TemperatureMonitor/Temperature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemperatureMonitor
+{
+    public class Temperature
+    {
+        public DateTime RecordDate { get; set; }
+        public int Data { get; set; }
+    }
+}
