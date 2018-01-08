a86b1ab5f959e30fb86fe8b7a259e9c31199129e
newli <newlifechou.outlook.com>
2018-01-08 14:35:35 +0800

    modify more

--- TemperatureMonitor/Form1.cs
+++ TemperatureMonitor/Form1.cs
@@ -20,49 +20,54 @@ namespace TemperatureMonitor
 
             temperatureGraph1.MachineName = "设备A";
             temperatureGraph1.MonitorPosition = "底部";
-            //temperatureGraph2.MachineName = "无";
-            //temperatureGraph2.MonitorPosition = "无";
-            //temperatureGraph3.MachineName = "无";
-            //temperatureGraph3.MonitorPosition = "无";
-            //temperatureGraph4.MachineName = "无";
-            //temperatureGraph4.MonitorPosition = "无";
 
-            helper = new DataProcessHelper();
+            dataHelper = new DataProcessHelper();
+            //开启一个任务
             Task.Factory.StartNew(StartTemperature1);
         }
 
 
         public void StartTemperature1()
         {
-            var operation = new SerialPortOperate("COM5");
-            while (true)
+            try
             {
-                //读取温度传感器的温度指令（未校验）
-                string readCmd = "010303200004";
-                operation.Write(readCmd);
-                Thread.Sleep(2000);
-                string hexString = operation.Read();
-                string tempstr = hexString.Substring(6, 4);
-                double temp = Convert.ToInt32(tempstr, 16);
-                THData d = new THData();
-                d.Temperature = temp;
-                d.Humidity = 0;
-                //d.CurrentTime = DateTime.Now;
-
-                int temperature1 = (int)d.Temperature;
-                //update ui
-                this.Invoke(new Action(() =>
+                var operation = new SerialPortOperate("COM5");
+                //一直循环
+                while (true)
                 {
-                    temperatureGraph1.SetCurrentTempareture(temperature1,d.CurrentTime);
-                }));
+                    //读取温度传感器的温度指令（未校验）
+                    string readCmd = "010303200004";
+                    operation.Write(readCmd);
+
+                    Thread.Sleep(2000);
+
+                    string hexString = operation.Read();
+                    string tempstr = hexString.Substring(6, 4);
+                    double temp = Convert.ToInt32(tempstr, 16);
+                    THData d = new THData();
+                    d.Temperature = temp;
+                    d.Humidity = 0;
+                    //d.CurrentTime = DateTime.Now;
+
+                    int temperature1 = (int)d.Temperature;
+                    //update ui
+                    this.Invoke(new Action(() =>
+                    {
+                        temperatureGraph1.SetCurrentTempareture(temperature1, d.CurrentTime);
+                    }));
 
 
-                //Write Log
-                helper.WriteData("A", temperature1);
+                    //Write Log
+                    dataHelper.WriteData("A", temperature1);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.Message);
             }
         }
 
-        private DataProcessHelper helper;
+        private DataProcessHelper dataHelper;
 
     }
 }
